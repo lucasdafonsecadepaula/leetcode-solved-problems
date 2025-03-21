f56b84e0a59b71be52d887435137c9a5fb06ac25
Lucas de Paula <lucasdafonsecadepaula@gmail.com>
2025-03-21 15:25:32 -0300

    Add 392 solution

---
+++ problems/392. Is Subsequence/solution.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/is-subsequence/description/
+
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isSubsequence(string s, string t)
+    {
+        if (s.size() > t.size())
+            return false;
+
+        int i = 0;
+        int j = 0;
+
+        while (i < s.size() && j < t.size())
+        {
+            if (s[i] == t[j])
+            {
+                i++;
+            }
+            j++;
+        }
+
+        if (i == s.size())
+            return true;
+
+        return false;
+    }
+};
